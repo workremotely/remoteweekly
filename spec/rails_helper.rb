25288eea9f27db27764c9dc6e2f907d184d5603b
Juanito Fatas <katehuang0320@gmail.com>
2015-10-10 02:07:22 +0800

    Setup dev environment and test suite

---
+++ spec/rails_helper.rb
@@ -0,0 +1,27 @@
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require "spec_helper"
+require "rspec/rails"
+
+require "capybara/rails"
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+
+  config.use_transactional_fixtures = false
+
+  config.before :suite do
+    DatabaseRewinder.clean_all
+  end
+
+  config.after :each do
+    DatabaseRewinder.clean
+  end
+end
